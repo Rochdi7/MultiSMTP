3ce89c09aec51b8540d98e8dbf795076a1b2b736
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-05-03 10:41:46 +0100

    add migration of smtp accounts

---
+++ database/migrations/2025_05_03_093305_create_smtp_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('smtp_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('driver')->default('smtp');
+            $table->string('host');
+            $table->string('port');
+            $table->string('encryption')->nullable();
+            $table->string('username');
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('smtp_accounts');
+    }
+};
