95b5bbacfbe3c5e2951d5341924cc44fd02f7c50
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-05-03 11:03:50 +0100

    updated web.php ROutes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MailController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [MailController::class, 'showForm'])->name('mail.form');
+Route::post('/send-email', [MailController::class, 'send'])->name('send.email');
